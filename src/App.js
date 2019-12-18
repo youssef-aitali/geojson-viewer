3391673165c7ea582c89a1b6ce1612e7c8532980
youssef93gis <youssef.aitali@outlook.com>
2019-12-18 10:35:09 +0100

    footer deleted

--- src/App.js
+++ src/App.js
@@ -4,14 +4,30 @@ import TopNavbar from './components/navbar/topnavbar.component';
 import Map from './components/map/map.component';
 import Footer from './components/footer/footer.component';
 
-function App() {
-	return (
-		<div className="main">
-			<TopNavbar />
-			<Map />
-			<Footer />
-		</div>
-	);
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			showSidebar: false
+		};
+	}
+
+	toggleSidebar = () => {
+		this.setState = ({
+			showSidebar: !this.state.showSidebar
+		},
+		console.log('Updated'));
+	};
+
+	render() {
+		return (
+			<div className="main">
+				<TopNavbar showSidebar={this.toggleSidebar} />
+				<Map />
+				{/* <Footer /> */}
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/navbar/topnavbar.component.jsx
+++ src/components/navbar/topnavbar.component.jsx
@@ -7,30 +7,48 @@ import {
   faLayerGroup
 } from '@fortawesome/free-solid-svg-icons';
 
-const TopNavbar = () => (
+const TopNavbar = ({ showSidebar }) => (
   <nav className="nav d-flex justify-content-between">
     <a className="nav-link active" href="#">
       Geoportal
     </a>
-    <form>
+    <form className="w-25">
       <input
         type="search"
         className="form-control"
         id="exampleInputEmail1"
         aria-describedby="emailHelp"
-        placeholder="Enter email"
+        placeholder="Search adresses, parcels, layers ..."
       />
     </form>
     <ul className="nav">
-      <li className="nav-item">
+      <li className="nav-item" onClick={showSidebar}>
         <a className="nav-link" href="#">
           <FontAwesomeIcon icon={faLayerGroup} />
         </a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">
+        <a
+          className="nav-link"
+          href="#"
+          id="dropdownMenuButton"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
           <FontAwesomeIcon icon={faGlobe} />
         </a>
+        <div
+          class="dropdown-menu dropdown-menu-right"
+          aria-labelledby="dropdownMenuButton"
+        >
+          <a class="dropdown-item" href="#">
+            FR
+          </a>
+          <a class="dropdown-item" href="#">
+            EN
+          </a>
+        </div>
       </li>
       <li className="nav-item">
         <a
@@ -55,7 +73,7 @@ const TopNavbar = () => (
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">
-              Modal title
+              My account
             </h5>
             <button
               type="button"
@@ -66,18 +84,44 @@ const TopNavbar = () => (
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <div class="modal-body">...</div>
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-secondary"
-              data-dismiss="modal"
-            >
-              Close
-            </button>
-            <button type="button" class="btn btn-primary">
-              Save changes
-            </button>
+          <div class="modal-body">
+            <form>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
+                />
+                <small id="emailHelp" class="form-text text-muted">
+                  We'll never share your email with anyone else.
+                </small>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Password"
+                />
+              </div>
+              <div class="form-group form-check">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  id="exampleCheck1"
+                />
+                <label class="form-check-label" for="exampleCheck1">
+                  Remember me
+                </label>
+              </div>
+              <button type="submit" class="btn btn-primary">
+                Submit
+              </button>
+            </form>
           </div>
         </div>
       </div>
